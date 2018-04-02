40551e8c911a146eb8d75ba10f95727d7c2c9ad6
Victor Guerrero Bosch <vguerrerobosch@gmail.com>
2018-04-02 12:18:11 +0200

    clean up methods no longer necessary and improve menu locations

--- wp-api-menus.php
+++ wp-api-menus.php
@@ -30,33 +30,34 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly
 }
 
+require_once 'includes/class-walker-rest-menu.php';
 // WP API v1.
 include_once 'includes/wp-api-menus-v1.php';
 // WP API v2.
 include_once 'includes/wp-api-menus-v2.php';
 
-if ( ! function_exists ( 'wp_rest_menus_init' ) ) :
+if (! function_exists('wp_rest_menus_init')) :
 
-	/**
-	 * Init JSON REST API Menu routes.
-	 *
-	 * @since 1.0.0
-	 */
-	function wp_rest_menus_init() {
+    /**
+     * Init JSON REST API Menu routes.
+     *
+     * @since 1.0.0
+     */
+    function wp_rest_menus_init()
+    {
+        if (! defined('JSON_API_VERSION') && ! in_array('json-rest-api/plugin.php', get_option('active_plugins'))) {
+            $class = new WP_REST_Menus();
+            add_filter('rest_api_init', array( $class, 'register_routes' ));
+        } else {
+            $class = new WP_JSON_Menus();
+            add_filter('json_endpoints', array( $class, 'register_routes' ));
+        }
+    }
 
-        if ( ! defined( 'JSON_API_VERSION' ) && ! in_array( 'json-rest-api/plugin.php', get_option( 'active_plugins' ) ) ) {
-			$class = new WP_REST_Menus();
-			 add_filter( 'rest_api_init', array( $class, 'register_routes' ) );
-		} else {
-			$class = new WP_JSON_Menus();
-			add_filter( 'json_endpoints', array( $class, 'register_routes' ) );
-		}
-	}
-
-	add_action( 'init', 'wp_rest_menus_init' );
+    add_action('init', 'wp_rest_menus_init');
 
 endif;
